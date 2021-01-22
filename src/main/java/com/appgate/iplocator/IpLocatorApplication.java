1b7d7cd8d37a9c88ef3f5bf9e6f4b85b36d668dc
accusor <hjdiazl@unal.edu.co>
2021-01-22 18:31:47 -0500

    Inicio de Proyecto

---
+++ src/main/java/com/appgate/iplocator/IpLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.iplocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpLocatorApplication.class, args);
+	}
+
+}
